be6400439fdf2a606b7a5e7a03072d4c3069950c
sangeet2021 <stiletdurden252@gmail.com>
2026-04-10 00:37:18 +0545

    feat: added a basic GIN endpoint to get started to the backend

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(".env file not found!")
+	}
+
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "hell yeah",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Server is running on port %s", port)
+	r.Run(":" + port)
+}
