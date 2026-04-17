71dd7d40b7a44178780d0ce9ba102ab4c33c8201
sangeet2021 <stiletdurden252@gmail.com>
2026-04-17 23:54:44 +0545

    feat: adding auth middleware to validate token

---
+++ backend/internal/auth/middleware.go
@@ -0,0 +1,46 @@
+package auth
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+    "github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == ""{
+			c.JSON(http.StatusUnauthorized, gin.H {"error" : "Authorization header required"})
+			c.Abort()
+			return 
+		}
+
+		parts := strings.Split(authHeader, "")
+		if len(parts) != 2 || parts[0] == "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error" : "Authorization format must be 'Bearer <token>'"})
+			c.Abort()
+			return
+		}
+
+		tokenString := parts[1]
+
+		token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error" : "Inavlid or expired token key"})
+			c.Abort()
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			userId := int(claims["userId"].(float64))
+			c.Set("userId", userId)
+		}
+
+		c.Next()
+	}
+}
