4af433dbd352accf29f59dc2f4c395bf2ed190ff
sangeet2021 <stiletdurden252@gmail.com>
2026-04-22 14:50:20 +0545

    fix: added new origins

--- backend/main.go
+++ backend/main.go
@@ -32,7 +32,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, 
+		AllowOrigins:     []string{"http://localhost:5173", "https://base62-seven.vercel.app"}, 
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
