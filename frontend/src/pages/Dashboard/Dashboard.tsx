fccc8d5eefed24c4101b39a776a7d48f7d42b819
sangeet2021 <stiletdurden252@gmail.com>
2026-04-22 23:50:09 +0545

    fix: minor code fixes

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -3,7 +3,6 @@ import { useAuthStore } from '@/store/AuthStore';
 import Button from '@/components/ui/Button';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
-import mht1 from '/mht/mht1.jpeg';
 
 const Dashboard: React.FC = () => {
   const navigate = useNavigate();
@@ -16,7 +15,6 @@ const Dashboard: React.FC = () => {
   };
   return (
     <div className="flex flex-col gap-16 items-center justify-center min-h-screen">
-      <img src={mht1} alt="" className="h-140 hover:scale-110 transition-all" />
       <Button onClick={handleLogoout}>Logout</Button>
     </div>
   );
